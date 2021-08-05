d22b3f0ed42577d691b4dcf1c29596f7e58c7e06
Mariano-UC <84363621+Mariano-UC@users.noreply.github.com>
2021-08-04 21:57:47 -0600

    Log in test, not running

---
+++ src/test/java/TestAccount.java
@@ -0,0 +1,34 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestAccount {
+    @Test
+    public void Test_Login_Succesful(){
+
+        String username = "mariano12castro@hotmail.com";
+        String password = "Password123";
+
+        String pathToDriver = Test.class.getResource("/chromedriver").getPath();
+        System.setProperty("webdriver.chrome.driver", pathToDriver);
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://demo.opencart.com/");
+
+        //Go to Log in page
+        driver.findElement(By.xpath("//*[@id='top-links']/ul/li[2]/a/span[1]")).click();
+        driver.findElement(By.linkText("Login")).click();
+
+        //Lenar Formulario
+        driver.findElement(By.name("email")).sendKeys("mariano12castro@hotmail.com");
+        driver.findElement(By.name("password")).sendKeys("Password123");
+        driver.findElement(By.cssSelector("[value='Login']")).click();
+
+        WebElement logOutButton = driver.findElement(By.linkText("Logout"));
+        Assert.assertTrue(logOutButton.isDisplayed());
+
+    }
+}
